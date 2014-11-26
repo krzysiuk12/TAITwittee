28ff5d8f46439e6cc6ca4c13cdd60caa851a86ec
Krzysztof Kicinger <kkrzysiu@poczta.onet.pl>
2014-11-26 17:18:24 +0100

    [Configuration] Simple Redirect (ShowEventsList - ShowEventDescription)

--- src/main/java/pl/edu/agh/beans/events/ShowEventDescriptionBean.java
+++ src/main/java/pl/edu/agh/beans/events/ShowEventDescriptionBean.java
@@ -1,7 +1,12 @@
 package pl.edu.agh.beans.events;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
 /**
  * Created by Krzysztof Kicinger on 2014-11-25.
  */
+@ManagedBean(name = "showEventDescriptionBean")
+@RequestScoped
 public class ShowEventDescriptionBean {
 }
--- src/main/java/pl/edu/agh/beans/events/ShowEventsListBean.java
+++ src/main/java/pl/edu/agh/beans/events/ShowEventsListBean.java
@@ -1,9 +1,21 @@
 package pl.edu.agh.beans.events;
 
+import pl.edu.agh.beans.navigation.NavigationResults;
+import pl.edu.agh.domain.Event;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
 /**
  * Created by Krzysztof Kicinger on 2014-11-25.
  */
+@ManagedBean(name = "showEventsListBean")
+@ViewScoped
 public class ShowEventsListBean {
+
+    public String goToEventDescription(Event event) {
+        return NavigationResults.SHOW_EVENT_DESCRIPTION_PAGE.getNavigation();
+    }
 
 
 }
